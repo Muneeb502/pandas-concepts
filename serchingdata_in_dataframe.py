976a4934654c5f3abd67e3fb769f3ffe3354de35
Muneeb502 <mu0476080@gmail.com>
2024-05-18 02:22:46 +0500

    adding new file calling data in raw from data

---
+++ serchingdata_in_dataframe.py
@@ -0,0 +1,20 @@
+"""
+in this file i am going to calling data in raw using mthod and also adding myown serial number using series
+
+"""
+import pandas as pd
+## creating different list in  dictonary
+mydic ={ 
+"id" :['1a', '2b', '3c'],
+"name" :['Anna', 'Peter', 'John'],
+"year" :  [1999, 2001, 1993]
+}
+
+mydata = pd.DataFrame(mydic , index=[a for a in range(1,len(mydic)+1)] )
+
+print(mydata)
+
+print("===================calling data from raw===========================")
+
+
+print(mydata.loc[2])
