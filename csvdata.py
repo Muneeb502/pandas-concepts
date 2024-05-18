cb54eef4e2f13dcca9b84a9f1582e32c7a51e965
Muneeb502 <mu0476080@gmail.com>
2024-05-19 01:51:27 +0500

    added

--- csvdata.py
+++ csvdata.py
@@ -5,4 +5,12 @@ print("in this file i ma going to understand the basic condets of csv file")
 
 df = pd.read_csv("CSV-FILECSV.csv")
 
-print(df)
+## find the shpe of  data means   find the numbers of raw and numbers of colunms
+print("find the shpe of  data means   find the numbers of raw and numbers of colunms")
+
+print(df.shape)
+
+## find the info of  data means  it will show me the numbers of colums and raw and also data type 
+print("find the info of  data means  it will show me the numbers of colums and raw and also data type ")
+
+print(df.info())
