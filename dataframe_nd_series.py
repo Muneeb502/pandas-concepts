58200fcc0b8b8a7efd254ce65ea335c1b238a616
Muneeb502 <mu0476080@gmail.com>
2024-05-19 14:47:10 +0500

    add data into dic with extend method

--- dataframe_nd_series.py
+++ dataframe_nd_series.py
@@ -2,5 +2,31 @@ import pandas as  pd
 import numpy as np
 
 dic = {
-    
+    "name": ["ali"],
+           
+    "age": [10], 
+
+     "city": ["kasmir"]
 }
+##adding new item in list dictnoray
+
+dic["name"].extend(["manzoor","bilal","moiz","afzaal","tariq","muneeb","usama","ubaid","arfa","zain","zuhaid"])
+
+dic["age"].extend([20,56,12,45,23,22,21,28,21,34,35])
+
+dic["city"].extend(["Karachi","Lahore","Faisalabad",'Rawalpindi','Gujranwala','Peshawar','Multan','Islamabad','Quetta','Bahawalpur',"gojra"])
+print(len(dic["age"]))
+print(len(dic["name"]))
+
+print(len(dic["city"]))
+
+"""
+
+
+making dictnoary into dataframe wth the help of pandas 
+
+
+ """
+
+d = pd.DataFrame(dic)
+print(d)
