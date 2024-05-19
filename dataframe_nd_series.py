c25dc4bfb67f9f2b8d37aac41aa92ef9f52a7114
Muneeb502 <mu0476080@gmail.com>
2024-05-19 15:10:54 +0500

    arranged col in data

--- dataframe_nd_series.py
+++ dataframe_nd_series.py
@@ -15,18 +15,18 @@ dic["name"].extend(["manzoor","bilal","moiz","afzaal","tariq","muneeb","usama","
 dic["age"].extend([20,56,12,45,23,22,21,28,21,34,35])
 
 dic["city"].extend(["Karachi","Lahore","Faisalabad",'Rawalpindi','Gujranwala','Peshawar','Multan','Islamabad','Quetta','Bahawalpur',"gojra"])
-print(len(dic["age"]))
-print(len(dic["name"]))
-
-print(len(dic["city"]))
-
-
 
 ##adding more col into data 
 
+
 dic["courses"]= ['pyhton', 'webdevelopment' , 'datascience', 'pyhton ', 'GD' ,'fullter' , 'figma' , 'backenddevelpoer' , 'mernstack developer' , "data eng",'javasxript','java' ]
 
-print(len(dic["courses"]))
+#print(len(dic["age"]))
+#print(len(dic["name"]))
+#print(len(dic["city"]))
+#print(len(dic["courses"]))
+
+
 """
 making dictnoary into dataframe wth the help of pandas 
 
@@ -35,7 +35,9 @@ making dictnoary into dataframe wth the help of pandas
 d = pd.DataFrame(dic)
 print(d)
 
+print(d.columns)
 
+# arranged data col 
+d.columns = [[ 'name', 'courses', 'city','age']]
 
-
-
+print(d)
